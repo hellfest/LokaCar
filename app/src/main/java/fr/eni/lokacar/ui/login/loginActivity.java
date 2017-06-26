3b22269a37d186713ca83b13acab86b12b44e8ac
hellfest <philippe.goruchon@gmail.com>
2017-06-26 15:57:38 +0200

    Creation des objets du package modele

---
+++ app/src/main/java/fr/eni/lokacar/ui/login/loginActivity.java
@@ -0,0 +1,15 @@
+package fr.eni.lokacar.ui.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fr.eni.lokacar.R;
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
